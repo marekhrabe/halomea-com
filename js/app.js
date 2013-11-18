2dfbe2e443ba2803ffca41381a54bce08747cfc2
marekhrabe <marekhrabe@abdoc.net>
2013-11-18 11:02:46 +0100

    db moved to firebase, not so random anymore

--- js/app.js
+++ js/app.js
@@ -8,98 +8,15 @@ var monkey = document.getElementById('monkey');
 var first = true;
 var animating = false;
 var h1 = document.getElementsByTagName('h1')[0];
-var quotes = [
-'Halo Mea?',
-'Halo Bo?',
-'Dafuq?!',
-'Dafuq?!',
-'Dafuq?!',
-'Dafuq?!',
-'Dafuq?!',
-'WTF?!',
-'WTF?!',
-'WTF?!',
-'Nooo, to by šlo',
-'Heleeee',
-'Kde mám peňeženku?',
-'…',
-'Čus',
-'Kaaaamoška',
-'Ty si frajerka!',
-'Kolik? 7 z 10?',
-'Co mají dnes v Oříšku?',
-'Jdem do Oříšku',
-'Tys nebyl v Oříšku?',
-'Heeej, to je feeest dobre!',
-'Oříšek je cool',
-'Viděl jsi, co dal Vuček na Dribble?',
-'Viděl jsi, co dal Jackie na Dribble?',
-'Viděl jsi, co dal Charlie na Dribble?',
-'Viděl jsi, co dal Musho na Dribble?',
-'Viděl jsi, co dal Štefo na Dribble?',
-'Já nevim, nedával jsem pozor',
-'Nejde mi dribbble',
-'Nechci do Dishe, jdem do Moodu',
-'Jedu za Kristýnou',
-'Ja som King of Pho',
-'Znáš tohle?',
-'Hej a co katamarán?',
-'Nepůjdem na katamarán?',
-'Katamarán je drahej, kámo!',
-'To je absurdní',
-'helloiamvu',
-'Vzbuď mě za 20 minut',
-'Mám hlad',
-'Znáš Aperol Spritz?',
-'Heeej',
-'Áno',
-'Dem s Keeganem na pivo',
-'Přijde Tom Bartl?',
-'Dafuq, vždyť jsem spláchnul!',
-'Ty nemáš Spotify?',
-'Kdo chce ještě LTE?',
-'To je fest popiči',
-'To dává smysl',
-'Musíme to A/B testovat',
-'Dáme tam A/B test',
-'Koukni, kreslím sovu',
-'Čí je ten banán?',
-'Ty si kokot',
-'Dal bych si něco k jídlu.',
-'Co si mám dát k jídlu?',
-'Dáme burger?',
-'Jdu běhat',
-'Ten burger v Tavernu je úplně boží',
-'Teď jsem zapomněl, co jsem chtel říct',
-'Ja vím, co je OpenGraph',
-'Je to v kufru',
-'Zapněte mi facebook!',
-'Bude to zejtra...',
-'To nejde.',
-'Jojo, to je tranzistor. ',
-'Čo si?',
-'Čo ti jebe',
-'Dej play',
-'Měl jsi někdy chicken tikka masala?',
-'Mám rád chicken tikka masala',
-'Dafuq, ty nevíš co je remorkér?',
-'Katamarán je na vzduch',
-'Vučíno',
-'Hej, to je katamarán',
-'Čo? Čo? Čo?',
-'V Alze maj nejlepší kávu na světě!',
-'Je mi špatně',
-'Jdu se projít kolem bloku',
-'Ne, pivo fakt ne.',
-'V Oříšku nemaj mrazák',
-'Co je, Štefaaaano!',
-];
+var quotes = ['Halo Mea!'];
+var last = null;
 
 new Firebase('https://halomea.firebaseio.com/').on('value', function (data) {
     var q = data.val();
     for (var i in q) {
         quotes.push(q[i]);
     }
+    last = quotes.length - 1;
 });
 
 document.getElementById('vuit').addEventListener('click', function () {
@@ -107,6 +24,9 @@ document.getElementById('vuit').addEventListener('click', function () {
     if (animating) {
         return false;
     }
+
+    var quote = Math.random() > .3 ? last-- : Math.floor(quotes.length * Math.random());
+
     animating = true;
     monkey.classList.remove('active');
     h1.classList.remove('active');
@@ -114,7 +34,7 @@ document.getElementById('vuit').addEventListener('click', function () {
         monkey.classList.add('active');
     }, 0);
     setTimeout(function () {
-        h1.innerText = quotes[Math.floor(quotes.length * Math.random())];
+        h1.innerText = quotes[quote];
         h1.classList.add('active');
         monkey.classList.remove('active');
         animating = false;
